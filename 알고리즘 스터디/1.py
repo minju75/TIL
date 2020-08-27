49fc11d28eb5b9e057083c97b1a1cd0aaea3da26
천민주 <sdr9873@naver.com>
2020-08-27 09:31:35 +0900

    0826 study

--- 알고리즘 스터디/1.py
+++ 알고리즘 스터디/1.py
@@ -1,3 +1,18 @@
-n = int(input())
-m = [[str(input())] for _ in range(n)]
-print(m)
+for i in range(1):
+    n = int(input())
+    m = 100
+    a  = [list(map(int,input().split())) for _ in range(m)]
+    y = a[n-1].index(2)
+    cnt = 0
+    mov = 0
+    while (n-1) > 0:
+        if (move==0 or move==1) and y>0 and a[n-1][y-1]:
+            y -= 1
+            move = 1
+        elif (move==0 or move==2) and y<n-1 and a[n-1][y-1]:
+            y += 1
+            move = 2
+        else : 
+            (n-1) -= 1
+            move = 0
+    print('#%d %d' %(i, y))
