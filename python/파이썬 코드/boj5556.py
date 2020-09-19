f964eb9e49034102acce57d719020153e523caca
천민주 <sdr9873@naver.com>
2020-09-19 16:42:43 +0900

    0919 study

--- python/파이썬 코드/boj5556.py
+++ python/파이썬 코드/boj5556.py
@@ -1,19 +1,4 @@
 n = int(input())
-h = int(input())
+k = int(input())
 board = [[0]*n for _ in range(n)]
-k = 0
-m = 1
-while k < n//2+1:
-    for i in range(0+k, n-k):
-        for j in range(0+k, n-k):
-            board[i][j] = m
-    k += 1
-    if m == 3:
-        m = 1
-    else:
-        m += 1
-
-for q in range(h):
-    a, b = map(int, input().split())
-    num = board[a-1][b-1]
-    print(num)
+for i in range()
