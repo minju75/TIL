3a9aab9e65cc2f73f7224ac91ec9f64ca781df8f
천민주 <sdr9873@naver.com>
2020-12-01 15:14:46 +0900

    study

--- python/파이썬 코드/BOJ/boj 16964.py
+++ python/파이썬 코드/BOJ/boj 16964.py
@@ -1,10 +1,3 @@
 n = int(input())
 li = [list(map(int, input().split())) for _ in range(n-1)]
 ans = list(map(int, input().split()))
-left = [0]*(n+1)
-right = [0]*(n+1)
-for i in range(n-1):
-    if left[li[i][0]] == 0:
-        left[li[i][0]] = li[i][1]
-    else:
-
