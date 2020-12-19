e6217d52593ee51a2241055df15aa7827e40aae9
천민주 <sdr9873@naver.com>
2020-12-19 15:57:29 +0900

    algorithm

--- python/파이썬 코드/boj 2178.py
+++ python/파이썬 코드/boj 2178.py
@@ -1,28 +1,25 @@
-N, M = map(int, stdin.readline().split())
-# matrix 배열
-matrix = [stdin.readline().rstrip() for _ in range(N)]
-# 방문경로 배열
-visited = [[0]*M for _ in range(N)]
-# 좌/우/위/아래 방향 이동
-dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+def bfs(x, y):
+    global visited
+    dx = [1, -1, 0, 0]
+    dy = [0, 0, 1, -1]
+    q = [[x, y]]
+    visited[x][y] = 1
+    while q:
+        x = q[0][0]
+        y = q[0][1]
+        q.pop(0)
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m:
+                if arr[nx][ny] == 1 and visited[nx][ny] == 0:
+                    q.append([nx, ny])
+                    visited[nx][ny] = visited[x][y] + 1
 
-# BFS 경로 탐색
-# queue 방식 사용
-queue = [(0,0)]
-visited[0][0] = 1
 
-while queue:
-    x, y = queue.pop(0)
+n, m = map(int, input().split())
+arr = [list(map(int, input())) for _ in range(n)]
+visited = [[0]*m for _ in range(n)]
 
-    if x == N-1 and y == M-1:
-        # 최종 경로 도착
-        print(visited[x][y])
-        break
-
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if 0 <= nx < N and 0 <= ny < M:
-            if visited[nx][ny] == 0 and matrix[nx][ny] == '1':
-                visited[nx][ny] = visited[x][y] + 1
-                queue.append((nx,ny))
+bfs(0, 0)
+print(visited[n-1][m-1])
