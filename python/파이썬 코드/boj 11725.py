17fe2c01ae9e6b18f0c60db3832444b353cb10d8
천민주 <sdr9873@naver.com>
2020-12-23 13:28:42 +0900

    algorithm

--- python/파이썬 코드/boj 11725.py
+++ python/파이썬 코드/boj 11725.py
@@ -1,3 +1,29 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(1000000) # 최대재귀한도깊이 풀기
+
+def find_p(start, tree, parent):
+    for i in tree[start]:
+        if parent[i] == 0:
+            parent[i] = start
+            find_p(i, tree, parent)
+
+# def find_p(node_list, start, parent):
+#     stack = [start]
+#     while stack:
+#         node = stack.pop()
+#         for i in node_list[node]:
+#             if parent[i] == 0 :
+#                 parent[i] = node
+#     return parent
+
 n = int(input())
+tree = [[] for _ in range(n+1)] # 0을 제외해야 해서 n+1
+parent = [0 for _ in range(n+1)] # 0을 제외해야 해서 n+1/ 부모 리스트를 확인할 리스트
 for _ in range(n-1):
     a, b = map(int, input().split())
+    tree[a].append(b) # 서로간의 연결관계 확인
+    tree[b].append(a)
+# print(tree)
+find_p(1, tree, parent)
+print(*parent[2:])
--- python/파이썬 코드/boj 9372.py
+++ python/파이썬 코드/boj 9372.py
@@ -1,4 +1,35 @@
-n = int(input())
-for _ in range(n-1):
-    a, b = map(int, input().split())
-    
+import sys
+from collections import deque
+
+def bfs(v):
+    global cnt
+    queue.append(v)
+    visited[v] = 1
+    while queue:
+        v = queue.popleft()
+        for n in arr[v]:
+            if visited[n] == 0:
+                visited[n] = 1
+                cnt += 1
+                queue.append(n)
+
+
+T = int(sys.stdin.readline())
+for t in range(T):
+    N, M = map(int, sys.stdin.readline().split())
+    # arr = [[0] * (N+1) for _ in range(N+1)]
+    arr = [[] for _ in range(N+1)]
+    visited = [0] * (N+1)
+    visited[0] = 1
+    cnt = 0
+    queue = deque([])
+    for m in range(M):
+        i, j = map(int, sys.stdin.readline().split())
+        arr[i].append(j)
+        arr[j].append(i)
+
+    for i in range(N):
+        if visited[i] == 0:
+            bfs(1)
+
+    print(cnt)
