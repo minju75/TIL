ec884c9c837c6dc16cb0729861eb1633a98789dd
minju75 <68409296+minju75@users.noreply.github.com>
2021-03-17 00:54:03 +0900

    ai_sub2

---
+++ vue 프론트엔드 연습/인공지능/front/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+
+    <v-main style="background-color: #192537;">
+      <HelloWorld/>
+    </v-main>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style>
+    * {
+    font-family: "MapoFlowerIsland";
+    color: white;
+  }
+  @font-face {
+    font-family: "MapoFlowerIsland";
+    src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2001@1.1/MapoFlowerIslandA.woff")
+      format("woff");
+    font-weight: normal;
+    font-style: normal;
+  }
+</style>
